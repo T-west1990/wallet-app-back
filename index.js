ae1e0e909972ad9fbf3856451687a8d881ca3038
Bizaza alain <34506923+T-west1990@users.noreply.github.com>
2025-01-21 03:38:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,13 +9,21 @@ const port = process.env.PORT || 8000;
 app.use(cors());
 app.use(express.json());
 
-// Database connection pool
-const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS || "", // Default to empty string if no password is provided
-  database: process.env.DB_NAME,
-  connectionLimit: 10, // Maximum number of connections in the pool
+// // Database connection pool
+// const db = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS || "", // Default to empty string if no password is provided
+//   database: process.env.DB_NAME,
+//   connectionLimit: 10, // Maximum number of connections in the pool
+// });
+
+//Database connection
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "wallet_app",
 });
 
 // Health check route
